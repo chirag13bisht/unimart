b0f18b1ded8bf8daabe0aa39c04b0dd98858f380
Atul Singh <getupmaverick@gmail.com>
2023-06-02 01:51:39 +0530

    fixed color scheme & home page

---
+++ static/index.js
@@ -0,0 +1,15 @@
+ScrollReveal({ 
+    reset: true,
+    distance: '600px',
+    duration: 2500,
+    delay: 400
+ });
+
+ ScrollReveal().reveal('.home-title, .work-title, .contact-data', { delay: 200 , origin: "left"});
+ ScrollReveal().reveal('.home-subtitle, .about-data', { delay: 200 , origin: "left"});
+ ScrollReveal().reveal('.home-img', { delay: 200 , origin: "right"});
+ ScrollReveal().reveal('.about-img, .list1, .list3, .list5, .copyright', { delay: 100 , origin: "right"});
+ ScrollReveal().reveal('.box1, .list2, .list4, .footer-social', { delay: 100 , origin: "left"});
+ ScrollReveal().reveal('.box2, .login-img', { delay: 200 , origin: "bottom"});
+ ScrollReveal().reveal('.box3, .home-matter', { delay: 200 , origin: "right"});
+ ScrollReveal().reveal('.contact-title', { delay: 200 , origin: "right"});
