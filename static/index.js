5c5368bb42c55e8fe5161405d9efc76d7f490de7
Atul Singh <getupmaverick@gmail.com>
2023-06-02 05:00:48 +0530

    modified home page

--- static/index.js
+++ static/index.js
@@ -1,15 +1,15 @@
 ScrollReveal({ 
     reset: true,
     distance: '600px',
-    duration: 2500,
+    duration: 1300,
     delay: 400
  });
 
- ScrollReveal().reveal('.home-title, .work-title, .contact-data', { delay: 200 , origin: "left"});
- ScrollReveal().reveal('.home-subtitle, .about-data', { delay: 200 , origin: "left"});
- ScrollReveal().reveal('.home-img', { delay: 200 , origin: "right"});
- ScrollReveal().reveal('.about-img, .list1, .list3, .list5, .copyright', { delay: 100 , origin: "right"});
- ScrollReveal().reveal('.box1, .list2, .list4, .footer-social', { delay: 100 , origin: "left"});
- ScrollReveal().reveal('.box2, .login-img', { delay: 200 , origin: "bottom"});
- ScrollReveal().reveal('.box3, .home-matter', { delay: 200 , origin: "right"});
- ScrollReveal().reveal('.contact-title', { delay: 200 , origin: "right"});
+ ScrollReveal().reveal('.home-title, .work-title, .contact-data', { delay: 100 , origin: "left"});
+ ScrollReveal().reveal('.home-subtitle, .intro-data', { delay: 100 , origin: "left"});
+ ScrollReveal().reveal('.home-img, .about-data', { delay: 100 , origin: "right"});
+ ScrollReveal().reveal('.list1, .list3, .list5, .copyright', { delay: 100 , origin: "right"});
+ ScrollReveal().reveal('.about-img, .box1, .list2, .list4, .footer-social', { delay: 100 , origin: "left"});
+ ScrollReveal().reveal('.box2, .login-img', { delay: 100 , origin: "bottom"});
+ ScrollReveal().reveal('.box3, .home-matter, .intro-img', { delay: 100 , origin: "right"});
+ ScrollReveal().reveal('.contact-title', { delay: 100 , origin: "right"});
